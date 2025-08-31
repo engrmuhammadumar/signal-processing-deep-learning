1e28640d27078116344773db678b1d1bc0fffa81
Muhammad Umar <19pwcse1758@uetpeshawar.edu.pk>
2025-08-31 14:58:52 +0900

    Add files via upload

---
+++ Implementation/config.py
@@ -0,0 +1,8 @@
+# config.py
+# Canonical class order (must match your dataset folders exactly)
+CLASSES = [
+    "Impeller (3.0BAR)",
+    "Mechanical seal Hole (3BAR)",
+    "Mechanical seal Scratch (3.0BAR)",
+    "Normal (3BAR)",
+]
